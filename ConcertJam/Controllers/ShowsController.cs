2ed7845d706d362ac41db4db77b1b9614a89fc94
Zenga Siwingwa <zengasiwingwa@outlook.com>
2018-03-11 18:03:12 -0400

    Added a dropdown list with data from DB in viewmodel

--- ConcertJam/Controllers/ShowsController.cs
+++ ConcertJam/Controllers/ShowsController.cs
@@ -1,13 +1,28 @@
-﻿using System.Web.Mvc;
+﻿using ConcertJam.Models;
+using ConcertJam.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace ConcertJam.Controllers
 {
     public class ShowsController : Controller
     {
         // GET: Shows
+        private ApplicationDbContext _context;
+
+
+        public ShowsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new ShowFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
         }
     }
 }
---
+++ ConcertJam/ViewModels/ShowFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+
+using ConcertJam.Models;
+using System.Collections.Generic;
+
+namespace ConcertJam.ViewModels
+{
+    public class ShowFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
