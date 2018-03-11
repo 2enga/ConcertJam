930ba05eca03eb2203bec1b6ac05aa7f303613de
Zenga Siwingwa <zengasiwingwa@outlook.com>
2018-03-11 19:23:21 -0400

    Post method for the create action

--- ConcertJam/Controllers/ShowsController.cs
+++ ConcertJam/Controllers/ShowsController.cs
@@ -1,5 +1,7 @@
 ﻿using ConcertJam.Models;
 using ConcertJam.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -16,6 +18,7 @@ namespace ConcertJam.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new ShowFormViewModel
@@ -23,6 +26,27 @@ namespace ConcertJam.Controllers
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(ShowFormViewModel viewModel)
+        {
+            var artistID = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == artistID);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            var show = new Show
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Shows.Add(show);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
