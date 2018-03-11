30e211c819174291e33895980115000472d21a2f
Zenga Siwingwa <zengasiwingwa@outlook.com>
2018-03-11 17:46:36 -0400

    Added a Create View with its controller

---
+++ ConcertJam/Controllers/ShowsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ConcertJam.Controllers
+{
+    public class ShowsController : Controller
+    {
+        // GET: Shows
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
